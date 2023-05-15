6d45ff0804c21445f05a01427805c6c622f47300
Dan Maffia <73266204+dan998@users.noreply.github.com>
2023-05-15 11:22:00 -0400

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,151 @@
+const body = document.body
+
+// boxes
+
+var box1 = document.querySelector('.box1')
+var box3 = document.querySelector('.box3')
+var box2 = document.querySelector('.box2')
+var box5 = document.querySelector('.box5')
+var box4 = document.querySelector('.box4')
+var box6 = document.querySelector('.box6')
+
+// events
+
+box1.addEventListener('click', () => {
+   var alert1 = window.confirm('do you want to continue?')
+   
+   if(alert1) {
+   // edit this 🙂
+        window.location.href = "./Games/Blackjack/dist/index.html"
+   }else {
+       alert("could not continue")
+   }
+});
+box2.addEventListener('click', () => {
+    var alert2 = window.confirm('Do You Want To Continue?')
+    
+    if(alert2) {
+    // edit this 🙂
+         window.location.href = "./Games/Duck-hunt/dist/index.html"
+    }else {
+        alert('could not continue')
+    }
+});
+box3.addEventListener('click?', () => {
+    var alert3 = confirm('Do You Want To Continue?')
+    
+    if(alert3) {
+    // edit this 🙂
+         window.location.href = "./Games/Hangman/dist/index.html"
+    }else {
+        alert('could not continue')
+    }
+});
+box4.addEventListener('click', () => {
+    var alert4 = confirm('Do You Want To Continue?')
+    
+    if(alert4) {
+    // edit this 🙂
+         window.location.href = "./Games/Sudoku/dist/index.html"
+    }else {
+        alert('could not continue')
+    }
+});
+box5.addEventListener('click', () => {
+    var alert5 = confirm('Do You Want To Continue?')
+    
+    if(alert5) {
+    // edit this 🙂
+         window.location.href = "./Games/Word Scrabble/index.html"
+    }else {
+        alert('could not continue')
+    }
+});
+box6.addEventListener('click', () => {
+    var alert6 = confirm('Do You Want To Continue?')
+    
+    if(alert6) {
+    // edit this 🙂
+         window.location.href = "./Games/Solitaire/dist/index.html"
+    }else {
+        alert('could not continue')
+    }
+});
+
+// THIS IS THE HAMBURGER SECTION
+
+const toggle = document.querySelector('#toggle');
+const show = document.querySelector('.show');
+
+toggle.onclick = () => {
+    show.classList.toggle('hide')
+};
+
+console.log(innerWidth);
+
+//nav onclick 🙂
+
+const list1 = document.querySelector('#list1')
+const list2 = document.querySelector('#list2')
+const list3 = document.querySelector('#list3')
+const list4 = document.querySelector('#list4')
+const list5 = document.querySelector('#list5')
+const list6 = document.querySelector('#list6')
+
+list1.onclick = () => {
+   const alert7 = confirm('Do You Want To Continue?')
+    if(alert7) {
+        window.location.href = "#"
+    }else {
+        alert('could not continue:(')
+    }
+};
+
+list2.onclick = () => {
+    const alert8 = confirm('would you like to continue?')
+    
+    if(alert8) {
+        window.location.href = "https://coding-family-portfolio.netlify.app/"
+    }else {
+        alert('could not continue:(')
+    }
+};
+
+list3.onclick = () => {
+    const alert9 = confirm('would you like to continue?')
+    
+    if(alert9) {
+        window.location.href = "#"
+    }else {
+        alert('could not continue:(')
+    }
+};
+
+list4.onclick = () => {
+     const jail = document.querySelector('.none')
+     jail.classList.toggle('free')
+};
+
+
+list5.onclick = () => {
+   const alert10 = confirm('Do You Want To Continue?')
+   
+   if(alert10) {
+       body.style.backgroundColor ="#000"
+       head.style.color ="#fff"
+   }else {
+       alert('Failed:(')
+   }
+};
+
+list6.onclick = () => {
+   const alert0 = confirm('Do You Want To Continue?')
+   
+   if(alert0) {
+       body.style.backgroundColor ="#fff"
+       head.style.color ="#000"
+   }else {
+       alert('Failed:(')
+   }
+       
+};
---
+++ src/media.js
@@ -0,0 +1,72 @@
+// media queries 😂
+
+const span1 = document.querySelector('.span1');
+const span2 = document.querySelector('.span2');
+const span3 = document.querySelector('.span3');
+const box0 = document.querySelector('#box0');
+const box$ = document.querySelector('#box2')
+const head = document.querySelector('#head');
+const big = document.querySelector('#big');
+const big2 = document.querySelector('.big');
+const card = document.querySelector(".card");
+const footer = document.querySelector("#foot")
+
+
+if(innerWidth >= 768) {
+    big.style.fontSize = "90px"
+    big2.style.borderBottom = "4px solid firebrick"
+    big2.style.height = "125px"
+    span1.style.width = '70px'
+    span2.style.width = "70px"
+    span3.style.width = "70px"
+    head.style.marginTop = "200px"
+    head.style.fontSize = "60px"
+    box0.style.marginLeft = "20px"
+    box$.style.marginRight = "20px"
+    span1.style.height = '10px'
+    footer.style.fontSize = "55px"
+    span2.style.height = '10px'
+    span3.style.height = '10px'
+    span1.style.transform = "translateY(-19px)"
+    span3.style.transform = "translateY(19px)"
+    toggle.style.transform = "translateY(20px)"
+    
+}else {
+    console.log(innerWidth);
+    big2.style.borderBottom = "4px solid firebrick"
+
+};
+
+if(innerWidth >= 600) {
+    card.style.background = "#000"
+    card.style.height ="500px"
+    card.style.scrollSnapType = 'x'
+    card.style.overflowX = "auto"
+    card.style.display = "flex"
+}else {
+    console.log("Coding family".toUpperCase());
+};
+
+/* NAV ONLY */
+
+if(innerWidth >= 768) {
+    show.style.textAlign = "left"
+    show.style.padding = "20px"
+    show.style.backgroundColor = "#000"
+    show.style.borderBottom = "4px solid firebrick"
+    
+}else {
+    console.log("Coding family".toUpperCase());
+    show.style.textAlign = "left"
+    show.style.backgroundColor = "#000"
+    show.style.borderBottom = "4px solid firebrick "
+};
+
+if(innerWidth >= 768) {
+    list6.onclick = () => {
+   const alert10 = confirm('Do You Want To Continue?')
+   alert10 ?  card.style.backgroundColor = "#444": console.log("coding family".toUpperCase());
+};
+}else {
+    console.log("codung family".toUpperCase());
+}
